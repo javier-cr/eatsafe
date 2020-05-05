cfcde4429c4bfd3c8ec9199da5aeec4ecd9487ad
Javier Ramirez <44209276+javier-cr@users.noreply.github.com>
2020-05-05 08:03:24 -0700

    Backend improvements

--- webroot/index.php
+++ webroot/index.php
@@ -9,8 +9,7 @@
     <meta name="theme-color" content="#ffffff">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="./bootstrap.min.css">
     <link rel="stylesheet" href="./customes.css">
     <title>Eat Safe: Trust Your Food</title>
 </head>
--- webroot/search.php
+++ webroot/search.php
@@ -9,8 +9,7 @@
     <meta name="theme-color" content="#ffffff">
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
-        integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="./bootstrap.min.css">
     <link rel="stylesheet" href="./customes.css">
     <title>Eat Safe Search</title>
 
@@ -25,21 +24,32 @@
             
             try {
 
-                $config = parse_ini_file('../db.ini');
+                // Bring in env vars & set charset
+                $dbhost = $_SERVER['RDS_HOSTNAME'];
+                $dbport = $_SERVER['RDS_PORT'];
+                $dbname = $_SERVER['RDS_DB_NAME'];
 
-                // Set up db connection
-                $dbh = new PDO('mysql:dbname=publichealth;host=localhost', $config['user'], $config['pass']);
+                $username = $_SERVER['RDS_USERNAME'];
+                $password = $_SERVER['RDS_PASSWORD'];
+
+                $charset = 'utf8';
+
+                // Create 1st part of connection string
+                $dsn = "mysql:host={$dbhost};port={$dbport};dbname={$dbname};charset={$charset}";
+                
+                // Assemble complete connection string
+                $dbh = new PDO($dsn, $username, $password);
+
                 // Send errors to php log, not on screen
                 $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-                $q = 'SELECT * FROM inspections WHERE 
-                `owner_name` LIKE :query OR 
-                `biz_name` LIKE :query OR
-                `address` LIKE :query OR
-                `city` LIKE :query OR
-                `state` LIKE :query OR
-                `zip` LIKE :query OR
-                `score` LIKE :query';
+                
+                $q = 'SELECT `program_name`, `facility_address`, `facility_city`, `facility_state`, 
+                `facility_zip` FROM restaurants WHERE 
+                `program_name` LIKE :query OR
+                `facility_address` LIKE :query OR
+                `facility_city` LIKE :query OR
+                `facility_state` LIKE :query OR
+                `facility_zip` LIKE :query';
 
                 // Prepare the SQL query string
                 $sth = $dbh->prepare($q);
@@ -71,7 +81,7 @@
                  */
                 http_response_code(500);
                 die('<h3>Whoops! Something went wrong. <small class="text-muted">Error 
-                establishing connection with database</small></h3>');
+                establishing connection with database</small></h3></head></html>');
             }
         }
 
@@ -82,7 +92,7 @@
              */
             http_response_code(400);
             die('<h3>Whoops! Something went wrong. <small class="text-muted">Error processing bad 
-            or malformed request</small></h3>');
+            or malformed request</small></h3></head></html>');
         }
     ?>
 
@@ -126,65 +136,51 @@
         </div>
     </div>
 
-    <!--Results table setup-->
-    <table class="table table-hover mx-auto col-10 col-md-8">
-        <div class="table responsive">
-            <!--Heading-->
-            <thead>
-                <tr>
-                    <th scope="col">Business Name</th>
-                    <th scope="col">Latest Score</th>
-                    <th scope="col">Latest Grade</th>
-                </tr>
-            </thead>
+    <div class="container">
+        <!--Filters-->
+        <div class="row">
+            <div class="col">
+                <!--revisit-->
+            </div>
+        </div>
+        <!--Results-->
+        <div class="row">
 
-            <!--Rows-->
-            <tbody>
+            <?php
+    foreach ($result as $item){ 
 
-                <?php
-            foreach ($result as $item){ 
+        // Create vars to reference later
+        $program_name = ucwords( strtolower($item["program_name"] ));
+        $address = ucwords( strtolower($item["facility_address"] ));
+        $city = ucwords( strtolower($item["facility_city"] ));
+        $state = ($item["facility_state"]);
+        $zip = ($item["facility_zip"]);
+        $score = "score_placeholder"; //($item["score"]);
+        $grade = "grade_placeholder"; //($item["grade"]);
 
-                // Create vars to reference later
-                $biz_name = ucwords( strtolower($item["biz_name"] ));
-                $address = ucwords( strtolower($item["address"] ));
-                $city = ucwords( strtolower($item["city"] ));
-                $state = ($item["state"]);
-                $zip = ($item["zip"]);
-                $score = ($item["score"]);
-                $grade = ($item["grade"]);
+        // Clickable Google Maps directions link for each item
+        $gmaps_url = "https://www.google.com/maps?saddr=My+Location&daddr=" . $item["program_name"] . "+" . $item["address"] . "+" . $item["city"] . "+" . $item["state"] . "+" . $item["zip"];
 
-                // Clickable Google Maps link for each item
-                $gmaps_url = "https://www.google.com/maps?saddr=My+Location&daddr=" . $item["biz_name"] . "+" . $item["address"] . "+" . $item["city"] . "+" . $item["state"] . "+" . $item["zip"];
+        // Table entry for each item
+        echo '
+            <div class="col">
+                <div class="card mx-auto" style="width: 44rem;">
+                    <div class="card-body">
+                        <h5 class="card-title">'.$program_name.'</h5>
+                        <h6 class="card-subtitle mb-2 text-muted">'.$address.', '.$city.', '.$state.' '.$zip.'</h6>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                        <a href="'.$gmaps_url.'" target="self" class="card-link">Directions</a>
+                        <a href="#" class="card-link">Another link</a>
+                    </div>
+                </div>
+            </div>
+            ';
+    }
+    ?>
 
-                // Table entry for each item
-                echo '
-                    <!--Start Row-->
-                    <tr>
-                        <td scope="row">
-                            <strong>
-                                '.$biz_name.'
-                            </strong>
-                            <br>
-                            <a href="'.$gmaps_url.'" target="self">
+        </div>
+    </div>
 
-                            '.$address.'
-                            <br>
-                            '.$city.', '.$state.' '.$zip.'
-                            </a>
-                        </td>
-
-                        <td>
-                            '.$item["score"].'
-                        </td>
-
-                        <td>
-                            '.$item["grade"].'
-                        </td>
-                    </tr>';
-            }
-        ?>
-            </tbody>
-    </table>
 </body>
 
 </html>
